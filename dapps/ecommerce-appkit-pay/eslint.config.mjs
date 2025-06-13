b41e5c5926b3e8b45bc7b494c8ba1c211280674b
Luka Isailovic <lukaisailovic@gmail.com>
2025-06-13 14:50:53 +0200

    chore: update lockfie and eslint

--- dapps/ecommerce-appkit-pay/eslint.config.mjs
+++ dapps/ecommerce-appkit-pay/eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "react-hooks/rules-of-hooks": "warn",
+      "react/no-unescaped-entities": "warn"
+    },
+  },
 ];
 
 export default eslintConfig;
